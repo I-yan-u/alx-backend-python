000823dc6aaa9a793f70f2f0f4b51580de1de990
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-06 01:03:39 +0100

    concat with annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Concat function with annotations """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concat function with annotations
+        Args:
+            str1 (str): first string to add
+            str2 (str): second string to add
+        return: (str1 + str2) str
+    """
+    return str1 + str2
