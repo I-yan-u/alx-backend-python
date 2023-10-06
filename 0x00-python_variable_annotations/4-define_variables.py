4ed552aca90d2e2a2faab70f38abce1ad93c80b0
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-06 01:20:30 +0100

    annotate variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 """ Define variables with annotations """
 
 
-a:int = 1
-pi:float = 3.14
-i_understand_annotations:bool = True
-school:str = 'Holberton'
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
