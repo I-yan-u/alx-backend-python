2d1391f5faad750cecd50b3feca7a43e5c1a2c6b
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-06 01:13:37 +0100

    to string func with annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" to_string function with annotations """
+
+
+def to_str(n: float) -> str:
+    """
+    to_str function with annotations
+        n (float): number to convert to string
+        return: string
+    """
+    return str(n)
