a87f47ea63e0035bf29b0cd2ef2b714577a940b5
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-06 01:07:49 +0100

    floor eith annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" floor function with annotations """
+
+
+def floor(n: float) -> int:
+    """
+    floor function with annotations
+        n (float): number to floor
+        return: integer
+    """
+    return int(n)
