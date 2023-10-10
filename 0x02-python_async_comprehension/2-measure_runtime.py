a3038c6b903ade65c2cced104a73dfcf612cef78
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-10 22:55:16 +0100

    measure async perfomance

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,12 @@
+""" Async Generators and comprehensions """
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ Async generators and comprehensions """
+    start = time.perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    stop = time.perf_counter() - start
+    return stop
