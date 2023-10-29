06eb6929dc0f98b0ca633d7871cd0b4bcca72014
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-10-29 09:31:23 +0000

    test memoized function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@ Unittest file
 """
 import unittest
 import utils
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 from unittest.mock import patch, MagicMock
 
@@ -45,6 +45,28 @@ class TestGetJson(unittest.TestCase):
 
         mock_request.assert_called_once_with(url)
         self.assertEqual(get_json(url), data)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test utils.memoize function
+    """
+    def test_memoize(self):
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == "__main__":
