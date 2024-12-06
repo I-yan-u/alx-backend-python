2151fbbf084cda5adeb7a1681478056926540207
I-yan-u <iyanuajimobi12@gmail.com>
2024-12-06 11:35:41 +0100

    Add batch processing and lazy pagination for user data retrieval

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+seed = __import__('seed')
+
+
+def stream_users_in_batches(batch_size):
+    cnx = seed.connect_to_prodev()
+    if cnx:
+        cursor = cnx.cursor()
+        cursor.execute(f"SELECT * FROM user_data LIMIT {batch_size}")
+        data = cursor.fetchall()
+        for row in data:
+            yield row
+
+
+def batch_processing(batch_size):
+    for rows in stream_users_in_batches(batch_size):
+        if rows[-1] <= 25:
+            continue
+        print(rows)
