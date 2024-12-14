38a4ec660ffcd6cfc21f20e43636734e06fe87b8
I-yan-u <iyanuajimobi12@gmail.com>
2024-12-14 12:35:39 +0100

    Refactor async user fetching functions to return results instead of printing

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -5,22 +5,27 @@ import aiosqlite
 async def async_fetch_users():
     async with aiosqlite.connect('users.db') as db:
        async with db.execute("SELECT * FROM users LIMIT 10") as cursor:
+           rows = []
            async for row in cursor:
-               print(row)
+               rows.append(row)
+           return rows
 
 
 async def async_fetch_older_users():
     async with aiosqlite.connect('users.db') as db:
         async with db.execute("SELECT * FROM users WHERE age > 40 LIMIT 10") as cursor:
-            async for row in cursor:
-               print(row)
+           rows = []
+           async for row in cursor:
+               rows.append(row)
+           return rows
 
 
 async def fetch_concurrently():
-    await asyncio.gather(
+    results = await asyncio.gather(
         async_fetch_users(),
         async_fetch_older_users()
     )
+    print(results)
 
 
 if __name__ == '__main__':
